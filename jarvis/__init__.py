0f047f036c615899572480bbce792b28e188c301
jarvis-mocap <jarvismocap@gmail.com>
2022-05-03 11:09:18 +0200

    changed lincense version

---
+++ jarvis/__init__.py
@@ -0,0 +1,6 @@
+"""
+JARVIS-MoCap (https://jarvis-mocap.github.io/jarvis-docs)
+Copyright (c) 2022 Timo Hueser.
+https://github.com/JARVIS-MoCap/JARVIS-HybridNet
+Licensed under GNU Lesser General Public License v2.1
+"""
--- jarvis/dataset/datasetBase.py
+++ jarvis/dataset/datasetBase.py
@@ -2,7 +2,7 @@
 JARVIS-MoCap (https://jarvis-mocap.github.io/jarvis-docs)
 Copyright (c) 2022 Timo Hueser.
 https://github.com/JARVIS-MoCap/JARVIS-HybridNet
-Licensed under GNU Lesser General Public License v3.0
+Licensed under GNU Lesser General Public License v2.1
 
 This implementation is strongly inspired and partially taken from pycocotools
 (https://github.com/cocodataset/cocoapi/tree/master/PythonAPI/pycocotools)
